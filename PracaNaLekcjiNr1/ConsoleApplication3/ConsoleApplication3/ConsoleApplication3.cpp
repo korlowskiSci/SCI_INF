726b84a33d0eff82b6e9eee40195149090e3060e
Karo <k.orlowski2117@gmail.com>
2021-11-03 00:32:41 +0100

    Poprawiona wersja

--- PracaNaLekcjiNr1/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ PracaNaLekcjiNr1/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <algorithm>
 #include <string>
 
@@ -8,13 +8,17 @@ string podstawieniowy(string a) {
 	int c;
 	cout << "O ile znakow przesunac litery: ";
 	cin >> c;
-	for (int i = 0; i < a.length(); i++) {
-		if (a[i] >= 97 && a[1] <= 122) {
-			a[i] = a[i] + c;
-			if (a[i] > 122) {
-				a[i] = a[i] % 123 + 97;
+	if(c > 0 && c <= 26){
+		for (int i = 0; i < a.length(); i++) {
+			if (a[i] >= 97 && a[1] <= 122) {
+				a[i] = a[i] + c;
+				if (a[i] > 122) {
+					a[i] = a[i] % 123 + 97;
+				}
 			}
 		}
+	}else{
+		cout << "Wprowadziles niepoprawna liczbe.";
 	}
 	return a;
 }
@@ -28,57 +32,48 @@ string przestawieniowy(string a) {
 	return a;
 }
 
-string cofnij1(string a) {
-
-	for (int i = 0; i < a.length(); i++) {
-		a[i] = a[i] + c;
-		if (a[i] < 97) {
-			a[i] = a[i] % 123 + 97;
+string odszyfruj(string a) {
+	for(int i = 0; i <= 26; i++){
+		for (int j = 0; j < a.length(); j++) {
+			if (a[j] >= 97 && a[j] <= 122) {
+				a[j] = a[j] - i;
+				if (a[j] < 97) {
+					a[j] = a[j] % 71 + 97;
+				}
+			}
 		}
+	cout << a << endl;
 	}
-	return a;
+	return 0;
 }
 
 int main()
 {
 	string tekst;
 	int b = 0;
-	char c;
 
 	cout << "Podaj ciag malych liter: ";
 	getline(cin, tekst);
 
-		cout << "\n1. Szyfr podstawieniowy(szyfr cezara).\n";
+		cout << "1. Szyfr podstawieniowy(szyfr cezara).\n";
 		cout << "2. Szyfr przestawieniowy.\n";
 		cout << "3. Szyfr podstawieniowy i szyfr przestawieniowy.\n";
-		cout << "4. Odszyfruj teskt.\n";
+		cout << "4. Odszyfruj tekst.\n";
 		cout << "Wybor: ";
 		cin >> b;
 
 		if (b == 1) {
 			cout << podstawieniowy(tekst);
-			cout << "Czy chcesz cofnac szyfr(t/n): ";
-			cin >> c;
-			if (c = 't') {
-				cout << cofnij1(tekst);
-			}
 		}
 		if (b == 2) {
 			cout << przestawieniowy(tekst);
-			cout << "Czy chcesz cofnac szyfr(t/n): ";
-			cin >> c;
-			if (c = 't') {
-				cout << cofnij2(tekst);
-			}
 		}
 		if (b == 3) {
 			cout << podstawieniowy(tekst);
 			cout << przestawieniowy(tekst);
-			cout << "Czy chcesz cofnac szyfr(t/n): ";
-			cin >> c;
-			if (c = 't') {
-				cout << cofnij3(tekst);
-			}
+		}
+		if (b == 4) {
+			cout << odszyfruj(tekst);
 		}
 	
 }
